5fc6881e539941c23c6ca00118efce59936afecc
Walter Ballauer <wballauer@aon.at>
2023-02-24 18:56:35 +0100

    Phase 1

--- src/Testdriver/testDriver.java
+++ src/Testdriver/testDriver.java
@@ -5,6 +5,7 @@ public class testDriver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Start with Testdriver");
+		System.out.println("Phase 1");
 
 	}
 
